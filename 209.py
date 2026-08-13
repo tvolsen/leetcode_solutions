17fb8657641dad32d96319e92e6b200a54d627d1
Trevor Olsen <75357108+tvolsen@users.noreply.github.com>
2026-08-13 17:05:53 -0400

    Add minSubArrayLen function to find subarray length
    Implement a solution to find the minimum length of a subarray with a sum greater than or equal to the target.

---
+++ 209.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        # ans, left pointer and current sum 
+        ans = float('inf')
+        left = 0
+        curr = 0
+        # iterate over entire array
+        for right in range(len(nums)):
+            # add right element to window
+            curr += nums[right]
+            # while window IS valid
+            while curr >= target:
+                # update ans
+                ans = min(ans, right - left + 1)
+                # remove left element and iterate left
+                curr -= nums[left]
+                left += 1
+        # no valid windows, return 0
+        if ans == float('inf'):
+            return 0
+        return ans
