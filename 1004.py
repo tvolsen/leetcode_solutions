99957c5cc7de5333b41dc2d12151012fd64707ad
Trevor Olsen <75357108+tvolsen@users.noreply.github.com>
2026-08-13 17:07:04 -0400

    Implement longestOnes function to find max length

---
+++ 1004.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        # ans, left pointer and num flips
+        ans = 0
+        left = 0
+        flips = 0
+        # iterate over entire array
+        for right in range(len(nums)):
+            # add right pointer to window
+            if nums[right] == 0:
+                flips += 1
+            # while window invalid, remove left and iterate
+            while flips > k:
+                if nums[left] == 0:
+                    flips -= 1
+                left += 1
+            # update ans by len of now valid window
+            ans = max(ans, right - left + 1)
+        return ans
