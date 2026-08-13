f755c2f45d3e461d8a27ff001b7b2f74a9aa32ce
Trevor Olsen <75357108+tvolsen@users.noreply.github.com>
2026-08-13 17:42:47 -0400

    Implement maxSubarrayLength method in Solution class

---
+++ 2958.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        counts = {}
+        ans = 0
+        left = 0
+        for right in range(len(nums)):
+            counts[nums[right]] = counts.get(nums[right], 0) + 1
+            while counts[nums[right]] > k:
+                counts[nums[left]] -= 1
+                left += 1
+            ans = max(ans, right - left + 1)
+        return ans
