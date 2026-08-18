9d42d1c8a7e09f871d486acd6e31d210c8611e69
Trevor Olsen <75357108+tvolsen@users.noreply.github.com>
2026-08-18 19:11:03 -0400

    Clarify comments in groupAnagrams method
    Updated comments for clarity on anagram grouping logic.

--- 49.py
+++ 49.py
@@ -1,16 +1,16 @@
 # 49. Group Anagrams
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # note, anagrams are equal if sorted
-        # previously seen strings, with keyed by sorted value
+        # note, two strings are anagrams if sorted values are equal
+        # previously seen strings, keyed by sorted value
         seen = {}
         ans = []
         for s in strs:
             # casts to list and sorts
             x = sorted(s)
             # casts back to sorted str
-            x = ''.join(x)
-            # if you have not seen sorted value, add to dict
+            x = "".join(x)
+            # if you have not seen sorted value, add to dict as list
             if x not in seen:
                 seen[x] = [s]
             # otherwise append it 
