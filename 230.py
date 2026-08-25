b110500b5fbfcd640cd96b7bd4197d6a9dd6831d
Trevor Olsen <75357108+tvolsen@users.noreply.github.com>
2026-08-25 13:03:42 -0400

    Add examples for kthSmallest method in 230.py
    Added commented-out examples for kthSmallest method using different approaches.

--- 230.py
+++ 230.py
@@ -18,3 +18,43 @@ class Solution:
                 return k, node.val
             return dfs(node.right, k)
         return dfs(root, k)[1]
+
+
+# # solution example using new object attributes to reflect globals (self.ans, self.k)
+# class Solution:
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+#         self.ans = None
+#         self.k = k
+#         def dfs(node):
+#             if not node:
+#                 return
+#             if self.ans is not None:
+#                 return
+#             dfs(node.left)
+#             self.k -= 1
+#             if self.k == 0:
+#                 self.ans = node.val
+#                 return
+#             dfs(node.right)
+#         dfs(root)
+#         return self.ans
+
+# # solution example using nonlocal to mimic globals
+# class Solution:
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+#         ans = None
+#         def dfs(node):
+#             nonlocal ans, k
+#             if not node:
+#                 return
+#             if ans is not None:
+#                 return
+#             dfs(node.left)
+#             k -= 1
+#             if k == 0:
+#                 ans = node.val
+#                 return
+#             dfs(node.right)
+#         dfs(root)
+#         return ans
+
