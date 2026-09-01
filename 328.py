f258824f8e26ca8002127ef2acc44fbd2b9324fc
Trevor Olsen <75357108+tvolsen@users.noreply.github.com>
2026-08-31 20:55:23 -0400

    Add solution for odd-even linked list problem
    Implement odd-even linked list rearrangement.

---
+++ 328.py
@@ -0,0 +1,22 @@
+# 328. Odd Even Linked List
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        odd = head
+        even = head.next
+        even_start = even
+
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        
+        odd.next = even_start
+        return head
